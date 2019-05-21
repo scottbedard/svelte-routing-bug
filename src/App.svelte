3609aa98c4fe5b74fa98ee54e0689753296e4878
Scott Bedard <scottbedard90@gmail.com>
2019-05-21 15:06:12 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<!-- App.svelte -->
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from './routes/Home.svelte';
+  import About from './routes/About.svelte';
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="about" component="{About}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
